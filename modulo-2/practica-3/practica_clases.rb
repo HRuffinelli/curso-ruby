7211e76c35f7eb8b7c0117926c25f6ed73d7c397
Hugo Ruffinelli <marceloruffinelli@gmail.com>
2016-10-22 12:29:45 -0300

    Modulo 2, Practica 3 - Uso de Clases con Getters y Setters

---
+++ modulo-2/practica-3/practica_clases.rb
@@ -0,0 +1,35 @@
+class Persona
+  #Persona debe tener atributos: nombre y apellido y deben ser accesibles
+  #Persona debe almacenar todas sus instancias para poder hacer seguimiento
+  attr_accessor :nombre, :apellido
+  @@personas = []
+
+  #Inicializador de instancias
+  def initialize(x,y)#Debe recibir dos parametros: nombre y apellido
+    @nombre = x
+    @apellido = y
+    @@personas.push self
+  end
+
+  #La clase debe tener tener un search por apellido
+  def self.search(apellido)
+    @@personas.select { |persona| persona.apellido == apellido }
+  end
+
+  #Tener un método que retorne el nombre y el apellido de la persona.
+  def to_s
+    "Nombre: #{nombre} - Apellido: #{apellido}"
+  end
+end
+
+#Esto es lo que va a hacer el rspec comentar para correr
+p1 = Persona.new("Juan", "Perez")
+p2 = Persona.new("Juan", "Benitez")
+p3 = Persona.new("Ermenindo", "Perez")
+p4 = Persona.new("Romina", "Benitez")
+
+puts Persona.search("Perez")
+
+# Debería imprimir
+# => Juan Perez
+# => Juana Perez
